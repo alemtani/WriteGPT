a4e9cb9e466e844ef55560d97002b7d8f9f36886
alemtani <alex.m.tanimoto@gmail.com>
2023-03-28 20:51:14 -0700

    Initialize frontend setup

--- src/App.js
+++ src/App.js
@@ -1,24 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
+  const works = [
+    {
+      id: '1',
+      title: 'Hello, world!',
+      genre: 'fiction',
+      body: 'This is a test work',
+      timestamp: 'a minute ago',
+      prompter: {
+        username: 'susan',
+      },
+    },
+    {
+      id: '2',
+      title: 'Second work',
+      genre: 'nonfiction',
+      body: 'This is a second test work',
+      timestamp: 'an hour ago',
+      prompter: {
+        username: 'john',
+      },
+    },
+  ];
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello World!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>WriteGPT</h1>
+      {works.length === 0 ?
+        <p>There are no works to display.</p>
+      :
+        works.map(work => {
+          return (
+            <p key={work.id}>
+              <b>{work.title}</b>
+              <br />
+              A work of {work.genre} prompted {work.timestamp} by <i>{work.prompter.username}</i>
+              <br />
+              {work.body}
+            </p>
+          );
+        })
+      }
+    </>
   );
 }
 
